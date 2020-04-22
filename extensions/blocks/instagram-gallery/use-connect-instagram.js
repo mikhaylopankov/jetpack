44a9155d1bae2e0efe4e4c33439b902e8b3687f1
Copons <Copons@users.noreply.github.com>
2020-04-22 11:49:58 +0100

    Instagram connection URL fetch error handling

--- extensions/blocks/instagram-gallery/use-connect-instagram.js
+++ extensions/blocks/instagram-gallery/use-connect-instagram.js
@@ -1,8 +1,9 @@
 /**
  * WordPress dependencies
  */
-import { useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
 
 /**
@@ -10,33 +11,42 @@ import { addQueryArgs } from '@wordpress/url';
  */
 import PopupMonitor from 'lib/popup-monitor';
 
-export default function useConnectInstagram( setAttributes, setImages ) {
+export default function useConnectInstagram( setAttributes, setImages, noticeOperations ) {
 	const [ isConnecting, setIsConnecting ] = useState( false );
 
 	const connectToService = () => {
+		noticeOperations.removeAllNotices();
 		setIsConnecting( true );
-		apiFetch( { path: `/wpcom/v2/instagram/connect-url` } ).then( connectUrl => {
-			const popupMonitor = new PopupMonitor();
 
-			popupMonitor.open(
-				connectUrl,
-				`connect-to-instagram-popup`,
-				'toolbar=0,location=0,menubar=0,' + popupMonitor.getScreenCenterSpecs( 700, 700 )
-			);
+		apiFetch( { path: `/wpcom/v2/instagram/connect-url` } )
+			.then( connectUrl => {
+				const popupMonitor = new PopupMonitor();
 
-			popupMonitor.on( 'message', ( { keyring_id } ) => {
-				setIsConnecting( false );
-				if ( keyring_id ) {
-					setAttributes( { accessToken: keyring_id.toString() } );
-				}
-			} );
+				popupMonitor.open(
+					connectUrl,
+					`connect-to-instagram-popup`,
+					'toolbar=0,location=0,menubar=0,' + popupMonitor.getScreenCenterSpecs( 700, 700 )
+				);
 
-			popupMonitor.on( 'close', name => {
-				if ( `connect-to-instagram-popup` === name ) {
+				popupMonitor.on( 'message', ( { keyring_id } ) => {
 					setIsConnecting( false );
-				}
+					if ( keyring_id ) {
+						setAttributes( { accessToken: keyring_id.toString() } );
+					}
+				} );
+
+				popupMonitor.on( 'close', name => {
+					if ( `connect-to-instagram-popup` === name ) {
+						setIsConnecting( false );
+					}
+				} );
+			} )
+			.catch( () => {
+				noticeOperations.createErrorNotice(
+					__( 'An error occurred. Please try again later.', 'jetpack' )
+				);
+				setIsConnecting( false );
 			} );
-		} );
 	};
 
 	const disconnectFromService = accessToken => {
